062a5fb4bf3cabc903b00670af75df1da7485c45
Brewster McCracken <mccrackenb@me.com>
2017-08-04 16:37:24 -0500

    Added sidebar
    Added code for sidebar
    Added jqery animations to move side bar in and out

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// Make it rain!
+$('#menuButton').on('click', function() {
+  $('main').addClass('contentShift');
+  $('.headerContent').addClass('contentShift');
+  $('.menuOpenOpacity').show();
+  $('nav').removeClass('sidebar');
+});
+$('.closeButton').on('click', function() {
+  $('main').removeClass('contentShift');
+  $('.headerContent').removeClass('contentShift');
+  $('.menuOpenOpacity').fadeOut('350ms');
+  $('nav').addClass('sidebar');
+})
